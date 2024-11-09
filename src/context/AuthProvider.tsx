160622ca469c8994445907b68e70c17330063b72
Anurag Samdariya <anurag4851@gmail.com>
2024-11-09 18:36:33 +0530

    final commit

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-import { SessionProvider } from "next-auth/react";
-import React from "react";
+import { SessionProvider } from 'next-auth/react';
 
 export default function AuthProvider({
-    children,
-
-}:{children: React.ReactNode}) {
-    return (
-        <SessionProvider>
-            {children}
-        </SessionProvider>
-    )
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  );
 }
+
+
+
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,13 +1,16 @@
-import {z} from "zod";
+import { z } from 'zod';
 
 export const usernameValidation = z
-    .string()
-    .min(8,"Username must be atleast 8 characters")
-    .max(14,"Username must be no more than 14 characters")
-    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain any special charcters")
+  .string()
+  .min(2, 'Username must be at least 2 characters')
+  .max(20, 'Username must be no more than 20 characters')
+  .regex(/^[a-zA-Z0-9_]+$/, 'Username must not contain special characters');
 
 export const signUpSchema = z.object({
-    username:usernameValidation,
-    email:z.string().email({message:"Invalid email address"}),
-    password:z.string().min(6,{message:"Password must be atleast 6 characters"})
-})
+  username: usernameValidation,
+
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters' }),
+});
